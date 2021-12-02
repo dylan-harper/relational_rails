58716d333bc4906170feaba90a25e6b911bba25e
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-02 14:43:44 -0600

    Route: elevatorco show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/elevatorcos', to: 'elevatorcos#index'
   get '/banks', to:'banks#index'
+  get '/elevatorcos/:id', to: 'elevatorcos#show'
 end
