fd1f555df9a8950d770c045e6715efcebc9c5954
Dylan Harper <dylanharper4@icloud.com>
2021-12-02 14:53:14 -0800

    Changed User Story 3 to Branches, not Branchs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_210313) do
+ActiveRecord::Schema.define(version: 2021_12_02_223837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_12_02_210313) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "branchs", force: :cascade do |t|
+  create_table "branches", force: :cascade do |t|
     t.string "name"
     t.integer "zip_code"
     t.boolean "has_atm"
