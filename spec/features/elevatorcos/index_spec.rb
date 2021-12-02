5727f67171743480122070ed1d593f88a16645f0
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-01 18:57:12 -0600

    Test: elevatorcos index

---
+++ spec/features/elevatorcos/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'the elevatorcos index page' do
+  it 'displays all elevatorcos' do
+    elevatorco = Elevatorco.create!(name: 'Vertical Transport',
+                                    address: '5624 Elevation Dr.',
+                                    num_technicians: 27,
+                                    offers_install?: true,
+                                    offers_service?: true,
+                                    offers_modernization?: true)
+    elevatorco2 = Elevatorco.create!(name: 'Ascend',
+                                     address: '9441 Skyscraper Ave.',
+                                     num_technicians: 16,
+                                     offers_install?: false,
+                                     offers_service?: true,
+                                     offers_modernization?: false)
+
+    visit "/elevatorcos"
+
+    expect(page).to have_content(elevatorco.name && elevatorco2.name)
+  end
+
+end
