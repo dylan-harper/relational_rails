93fb3e830bbba932e42c5162ddcb7b2bb0c73208
Dylan Harper <dylanharper4@icloud.com>
2021-12-01 23:22:50 -0800

    Add banks#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #User Story 1
   get '/elevatorcos', to: 'elevatorcos#index'
   get '/banks', to:'banks#index'
+  #User Story 2
+  get '/banks/:id', to: 'banks#show'
 end
