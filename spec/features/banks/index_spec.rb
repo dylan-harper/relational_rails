ddbc80ca4e85a8f883f6408385c06ce62e9ef043
Dylan Harper <dylanharper4@icloud.com>
2021-12-02 23:26:00 -0800

    Add user story 6 to bank#index

--- spec/features/banks/index_spec.rb
+++ spec/features/banks/index_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'bank index' do
-
-  it 'can dipslay all banks' do
-    bank_1 = Bank.create!(name: 'Wells Fargo',
+  before :each do
+    @bank_1 = Bank.create!(name: 'Wells Fargo',
                           hq_city_state: 'San Francisco, California',
                           fdic_ins: true,
                           mobile_app: true,
                           size_by_assets: 1303558000)
-    bank_2 = Bank.create!(name: 'Chase',
+    @bank_2 = Bank.create!(name: 'Chase',
                           hq_city_state: 'New York City, New York',
                           fdic_ins: true,
                           mobile_app: true,
                           size_by_assets: 3757576000)
+    @bank_3 = Bank.create!(name: 'BoA',
+                            hq_city_state: 'Charlotte, North Carolina',
+                            fdic_ins: true,
+                            mobile_app: true,
+                            size_by_assets: 3000000000)
+  end
+
+  it 'can dipslay all banks' do
     visit "/banks"
 
-    expect(page).to have_content(bank_1.name)
-    expect(page).to have_content(bank_2.name)
+    expect(page).to have_content(@bank_1.name)
+    expect(page).to have_content(@bank_2.name)
+  end
+
+  it 'orders records by recently created and lists created_at' do
+    visit "/banks"
+
+    expect(page).to have_content(@bank_1.created_at)
+    expect(page).to have_content(@bank_2.created_at)
+    expect(page).to have_content(@bank_3.created_at)
   end
 
 end
