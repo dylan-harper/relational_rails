872b7694c45966d23a5be62f280b57b79b015d06
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-03 23:38:06 -0600

    DB: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,43 @@
                                     zip_code: 654321,
                                     has_atm: false,
                                     quarterly_rev: 2000000)
+
+@elevatorco = Elevatorco.create!(name: 'Vertical Transport',
+                                address: '5624 Elevation Dr.',
+                                num_technicians: 27,
+                                offers_install?: true,
+                                offers_service?: true,
+                                offers_modernization?: true)
+@elevatorco2 = Elevatorco.create!(name: 'Ascend',
+                                  address: '9441 Skyscraper Ave.',
+                                  num_technicians: 16,
+                                  offers_install?: false,
+                                  offers_service?: true,
+                                  offers_modernization?: false)
+@building = Building.create!(name: 'Tropical Resort',
+                            address: '902 W Coconut Way',
+                            owner: 'Zed Colada',
+                            unit_type: 'passenger',
+                            num_units: 8,
+                            year_installed: '2018-01-01',
+                            needs_modernization?: false,
+                            last_serviced: '2021-10-16',
+                            elevatorco_id: @elevatorco.id)
+@building2 = Building.create!(name: 'Toothpick Factory',
+                              address: '46112 Point View',
+                              owner: 'Salvador Datun',
+                              unit_type: 'freight',
+                              num_units: '1',
+                              year_installed: '1996-01-01',
+                              needs_modernization?: true,
+                              last_serviced: '2021-10-16',
+                              elevatorco_id: @elevatorco2.id)
+@building3 = Building.create!(name: 'Native Roots',
+                            address: '408 Broadway',
+                            owner: 'Phil Big',
+                            unit_type: 'vertical platform lift',
+                            num_units: 1,
+                            year_installed: '2014-01-01',
+                            needs_modernization?: false,
+                            last_serviced: '2021-07-02',
+                            elevatorco_id: @elevatorco2.id)
