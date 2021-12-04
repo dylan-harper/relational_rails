f231d2a09acff38cb1f78dcfdd735e9cd4e3322e
Dylan Harper <dylanharper4@icloud.com>
2021-12-03 18:43:40 -0800

    Change branches_count to be in bank model

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,7 @@
 class Bank < ApplicationRecord
   has_many :branches
+
+  def branches_count
+    self.branches.count
+  end
 end
--- spec/features/banks/show_spec.rb
+++ spec/features/banks/show_spec.rb
@@ -42,24 +42,34 @@ RSpec.describe 'bank show' do
 
   before :each do
     @bank_1 = Bank.create!(name: 'Wells Fargo',
-                          hq_city_state: 'San Francisco, California',
-                          fdic_ins: true,
-                          mobile_app: true,
-                          size_by_assets: 1303558000)
+                           hq_city_state: 'San Francisco, California',
+                           fdic_ins: true,
+                           mobile_app: true,
+                           size_by_assets: 1303558000)
     @bank_2 = Bank.create!(name: 'Chase',
-                          hq_city_state: 'New York City, New York',
-                          fdic_ins: true,
-                          mobile_app: true,
-                          size_by_assets: 3757576000)
+                           hq_city_state: 'New York City, New York',
+                           fdic_ins: true,
+                           mobile_app: true,
+                           size_by_assets: 3757576000)
     @bank_3 = Bank.create!(name: 'BoA',
-                            hq_city_state: 'Charlotte, North Carolina',
-                            fdic_ins: true,
-                            mobile_app: true,
-                            size_by_assets: 3000000000)
+                           hq_city_state: 'Charlotte, North Carolina',
+                           fdic_ins: true,
+                           mobile_app: true,
+                           size_by_assets: 3000000000)
+    @branch_1 = @bank_1.branches.create!(name: "Branch",
+                                         zip_code: 123456,
+                                         has_atm: true,
+                                         quarterly_rev: 1000000)
+    @branch_2 = @bank_2.branches.create!(name: "Other Branch",
+                                         zip_code: 654321,
+                                         has_atm: false,
+                                         quarterly_rev: 2000000)
   end
+
   it 'shows the count of children' do
     visit "/banks/#{@bank_1.id}"
 
     expect(page).to have_content('Number of Branches: 1')
+    expect(page).to have_content(@bank_1.branches_count)
   end
 end
