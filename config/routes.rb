3cbf38b43d6948ef578dbe27879604f64ac51d8d
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-04 19:12:43 -0600

    Feature: add create elevatorco functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   #User Story 2
   get '/banks/:id', to: 'banks#show'
-  get '/elevatorcos/:id', to: 'elevatorcos#show'
+
+  #User Story 11
+  get '/elevatorcos/new', to: 'elevatorcos#new'
+  post '/elevatorcos', to: 'elevatorcos#create'
 
   #User Story 3
   get '/buildings', to: 'buildings#index'
@@ -20,4 +23,5 @@ Rails.application.routes.draw do
   get '/banks/:bank_id/branches', to: 'bank_branches#index'
   get 'elevatorcos/:elevatorco_id/buildings', to: 'elevatorco_buildings#index'
 
+  get '/elevatorcos/:id', to: 'elevatorcos#show'
 end
