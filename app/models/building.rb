05d25bc6b52105da5c77a57b28df066194aa994c
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-06 14:19:43 -0600

    Model: add method to sort alphabetically

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,6 +2,10 @@ class Building < ApplicationRecord
   belongs_to :elevatorco
 
   def self.display_true
-    Building.where(needs_modernization?: true)  
+    Building.where(needs_modernization?: true)
+  end
+
+  def self.sort_by_name
+    Building.order("name")
   end
 end
