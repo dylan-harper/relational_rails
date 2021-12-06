670fbbb42a7b154218ff0b773068b3023b3cd6d3
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-05 19:40:02 -0600

    Refactor: clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,45 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #User Story 1
+  #Parent Index
   get '/elevatorcos', to: 'elevatorcos#index'
   get '/banks', to:'banks#index'
 
+  #Parent Create
   get '/banks/new', to: 'banks#new'
-  get '/banks/:id/branches/new', to: 'bank_branches#new'
-  post '/banks/:id/branches', to: 'bank_branches#create'
   post '/banks', to:'banks#create'
-
-  #User Story 2
-  get '/banks/:id', to: 'banks#show'
-  get '/banks/:id/edit', to: 'banks#edit'
-  patch '/banks/:id', to: 'banks#update'
-
-  #User Story 11
   get '/elevatorcos/new', to: 'elevatorcos#new'
   post '/elevatorcos', to: 'elevatorcos#create'
 
-  #User Story 14
-  get '/buildings/:id/edit', to: 'buildings#edit'
-  patch '/buildings/:id', to: 'buildings#update'
-
-  #User Story 13
-  get '/elevatorcos/:id/buildings/new', to: 'elevatorco_buildings#new'
-  post '/elevatorcos/:id/buildings/', to: 'elevatorco_buildings#create'
-
-  #User Story 12
+  #Parent Update
+  get '/banks/:id/edit', to: 'banks#edit'
+  patch '/banks/:id', to: 'banks#update'
   get '/elevatorcos/:id/edit', to: 'elevatorcos#edit'
   patch '/elevatorcos/:id/', to: 'elevatorcos#update'
 
-  #User Story 3
+  #Child Index
   get '/buildings', to: 'buildings#index'
   get '/branches', to: 'branches#index'
 
-  #User Story 4
-  get '/buildings/:id', to: 'buildings#show'
-  get '/branches/:id', to: 'branches#show'
+  #Child Show
+  get '/banks/:id', to: 'banks#show'
+  get '/elevatorcos/:id', to: 'elevatorcos#show'
 
-  #User Story 5
+  #Child Update
+  get '/buildings/:id/edit', to: 'buildings#edit'
+  patch '/buildings/:id', to: 'buildings#update'
+
+  #Parent Child Index
   get '/banks/:bank_id/branches', to: 'bank_branches#index'
   get 'elevatorcos/:id/buildings', to: 'elevatorco_buildings#index'
 
-  get '/elevatorcos/:id', to: 'elevatorcos#show'
-  #User Story 11
-  # get '/banks/new', to: 'banks#new'
+  #Parent Child Show
+  get '/buildings/:id', to: 'buildings#show'
+  get '/branches/:id', to: 'branches#show'
+
+  #Parent Child Create
+  get '/banks/:id/branches/new', to: 'bank_branches#new'
+  post '/banks/:id/branches', to: 'bank_branches#create'
+  get '/elevatorcos/:id/buildings/new', to: 'elevatorco_buildings#new'
+  post '/elevatorcos/:id/buildings/', to: 'elevatorco_buildings#create'
+
 end
