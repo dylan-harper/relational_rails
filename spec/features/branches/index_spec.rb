3c6099f5fb6126e61c05b6851210b5e35fde54b3
Dylan Harper <dylanharper4@icloud.com>
2021-12-06 22:02:34 -0800

    Finish functionality for both user stories

--- spec/features/branches/index_spec.rb
+++ spec/features/branches/index_spec.rb
@@ -45,4 +45,20 @@ RSpec.describe 'branches index' do
     expect(page).not_to have_content(@branch_2.name)
   end
 
+  it 'can delete the bank from the index page' do
+    branch = @bank_1.branches.create!(name: 'First Street',
+                              zip_code: 123456,
+                              has_atm: true,
+                              quarterly_rev: 1000000,
+                              bank_id: 3)
+
+    visit '/branches'
+
+    click_button "Delete #{branch.name}"
+
+    expect(current_path).to eq('/branches')
+    expect(page).to_not have_content(branch.name)
+    expect(page).to have_content(@branch_1.name)
+  end
+
 end
